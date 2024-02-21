cb38649797c9eacf7e1c7e7b2e49e74ecae5d3d8
laperex <krishnadev7777@gmail.com>
2024-02-21 17:03:25 +0530

    tree create

--- src/main.c
+++ src/main.c
@@ -2,101 +2,83 @@
 #include <stdio.h>
 
 struct node {
-	struct node* prev;
-	struct node* next;
-	uint8_t data;
+	uint16_t leaf;
+	uint16_t tag;
+	uint16_t data;
+	uint16_t freq;
 };
+
+void swap_u16(uint16_t* a, uint16_t* b) {
+	uint16_t tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void swap_node(struct node* a, struct node* b) {
+	swap_u16(&a->data, &b->data);
+	swap_u16(&a->freq, &b->freq);
+	swap_u16(&a->leaf, &b->leaf);
+	swap_u16(&a->tag, &b->tag);
+}
 
 int main() {
 	const char* data = "ABCDEFGGGABS";
 	
-	uint8_t freq_array[256] = { 0 };
-	uint8_t freq_next_ptr_array[256] = { 0 };
-
-	uint8_t next_ptr_array[256] = { 0 };
-	uint8_t prev_ptr_array[256] = { 0 };
-
-	uint8_t sorted_array_size = 0;
-
-	uint8_t root = 0;
+	uint8_t idx_array[256] = { 0 };
 	
+	struct node condensed_array[256] = { 0 };
+	
+	uint8_t condensed_array_idx = 0;
+
 	for (int i =  0; data[i] != '\0'; i++) {
-		if (freq_array[data[i]] == 0) {
-			sorted_array_size += 1;
+		if (condensed_array[idx_array[data[i]]].data != data[i]) {
+			condensed_array[condensed_array_idx].data = data[i];
+			condensed_array[condensed_array_idx].leaf = 1;
+
+			idx_array[data[i]] = condensed_array_idx++;
 		}
 
-		// if (i == 0) {
-		// 	root = data[i];
-		// 	next_ptr_array[data[i]] = data[i];
-		// } else {
-		// 	uint8_t next = root;
-
-		// 	// while (freq_array[data[i]] >= freq_array[next]) {
-		// 	// 	next = next_ptr_array[next];
-		// 	// }
-
-		// 	// next_ptr_array[data[i]] = next;
-		// }
-
-		// prev_ptr_array[data[i]] = freq_next_ptr_array[freq_array[data[i]]];
-		// next_ptr_array[data[i]] = next_ptr_array[freq_next_ptr_array[freq_array[data[i]]]];
-		// freq_next_ptr_array[freq_array[data[i]]] = 
-
-		// if (freq_array[data[i]] == 0) {
-		// 	next_ptr_array[data[i]] = root;
-		// 	prev_ptr_array[root] = data[i];
-		// 	root = data[i];
-		// } else {
-		// 	if (freq_array[data[i]] >= freq_array[next_ptr_array[data[i]]]) {
-		// 		uint8_t p_a = prev_ptr_array[data[i]];
-		// 		uint8_t p_b = data[i];
-		// 		uint8_t p_c = next_ptr_array[data[i]];
-		// 		uint8_t p_d = next_ptr_array[next_ptr_array[data[i]]];
-
-		// 		if (freq_array[p_c]) {
-		// 			prev_ptr_array[p_b] = p_c;
-		// 			prev_ptr_array[p_c] = p_a;
-		// 			prev_ptr_array[p_d] = p_b;
-					
-		// 			next_ptr_array[p_a] = p_c;
-		// 			next_ptr_array[p_b] = p_d;
-		// 			next_ptr_array[p_c] = p_b;
-
-		// 			printf("%c - %c - %c - %c\n", p_a, p_b, p_c, p_d);
-	
-		// 			if (data[i] == root) {
-		// 				root = p_c;
-		// 			}
-		// 		}
-		// 	}
-		// }
-
-		// uint8_t next = root;
-		// for (int i = 0; i < sorted_array_size; i++) {
-		// 	if (i != 0)
-		// 		printf(" -> ");
-		// 	printf("%c", next);
-		// 	next = next_ptr_array[next];
-		// }
-		// printf("\n");
-
-		freq_array[data[i]] += 1;
+		condensed_array[idx_array[data[i]]].freq++;
 	}
 	
-	printf("sorted array size - %i\n", sorted_array_size);
-	printf("smallest - %c\n", root);
-	
-	for (int i = 0; i < 256; i++) {
-		if (freq_array[i]) {
-			printf("%c - %i - %c - %c\n", i, freq_array[i], prev_ptr_array[i], next_ptr_array[i]);
-		}
-	}
-	// uint8_t next = root;
-	// for (int i = 0; i < sorted_array_size; i++) {
-	// 	if (i != 0)
-	// 		printf(" -> ");
-	// 	printf("%c", next);
-	// 	next = next_ptr_array[next];
+	// for (int i = 0; i < condensed_array_idx; i++)	{
+	// 	printf("%c = %i\n", condensed_array[i].data, condensed_array[i].freq);
 	// }
-	// printf("\n");
+	
+	printf("sorted array size - %i\n", condensed_array_idx);
+
+
+	for (int i = 0; i < condensed_array_idx; i++) {
+		for (int j = i + 1; j < condensed_array_idx; j++) {
+			if (condensed_array[j].freq < condensed_array[i].freq) {
+				swap_node(&condensed_array[j], &condensed_array[i]);
+			}
+		}
+	}
+
+	uint8_t non_leaf_idx = 0;
+
+	for (uint8_t i = 1; i < condensed_array_idx + non_leaf_idx - 1; i += 2) {
+		if (condensed_array[i].freq <= condensed_array[condensed_array_idx + non_leaf_idx - 1].freq || i == 1) {
+			condensed_array[condensed_array_idx + non_leaf_idx].leaf = 0;
+			condensed_array[condensed_array_idx + non_leaf_idx].data = ((i - 1) << 8) | i;
+			condensed_array[condensed_array_idx + non_leaf_idx].freq = condensed_array[i - 1].freq + condensed_array[i].freq;
+			non_leaf_idx += 1;
+		} else {
+			condensed_array[condensed_array_idx + non_leaf_idx].leaf = 0;
+			condensed_array[condensed_array_idx + non_leaf_idx].data = ((i - 1) << 8) | (condensed_array_idx + non_leaf_idx - 1);
+			condensed_array[condensed_array_idx + non_leaf_idx].freq = condensed_array[i - 1].freq + condensed_array[condensed_array_idx + non_leaf_idx - 1].freq;
+			non_leaf_idx += 1;
+		}
+	}
+
+	for (int i = 0; i < condensed_array_idx + non_leaf_idx; i++)	{
+		if (condensed_array[i].leaf == 1) {
+			printf("\t%i\t %c     = %i\n", i, condensed_array[i].data, condensed_array[i].freq);
+		} else {
+			printf("\t%i\t %i - %i = %i\n", i, (condensed_array[i].data >> 8) & 255, condensed_array[i].data & 255, condensed_array[i].freq);
+		}
+	}
+
+	printf("tree array size - %i\n", condensed_array_idx + non_leaf_idx);
 }
