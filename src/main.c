762b41e99b169d88645676489dfe43da1cd56079
laperex <krishnadev7777@gmail.com>
2024-02-28 20:04:19 +0530

    fini

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 #define FREQ_BIT ((256 * 256) - 1)
+#define DATA_SIZE (12)
+#define NODE_PRINT "%c"
 
 void swap_u32(uint32_t* a, uint32_t* b) {
 	uint32_t tmp = *a;
@@ -57,7 +61,7 @@ void traverse(const uint32_t* root, uint16_t* result_array, int index, int level
 				printf("\t");
 			}
 
-			printf("| %i - %i", root[index] >> 17, root[index] & FREQ_BIT);
+			printf("| " NODE_PRINT " - %i", root[index] >> 17, root[index] & FREQ_BIT);
 
 			result_array[root[index] >> 17] = id;
 
@@ -71,86 +75,89 @@ void traverse(const uint32_t* root, uint16_t* result_array, int index, int level
 	}
 }
 
-#define DATA_SIZE (1000)
 
-int main() {
-	uint8_t data[DATA_SIZE] = "2e1284e1243                       q";
-	
-	for (uint32_t i = 0; i < DATA_SIZE; i++) {
-		data[i] = (i + 1) % 256;
-	}
+uint32_t init_tree_array(uint8_t* data, uint16_t* tree_index_array, uint32_t* tree_array, uint32_t* tree_array_idx) {
+	uint32_t i;
 
-	uint32_t data_size = 0;
-	
-	uint16_t idx_array[256] = { 0 };
+	for (i =  0; i < DATA_SIZE; i++) {
+		if ((tree_array[tree_index_array[data[i]]] >> 17) != data[i]) {
+			tree_array[*tree_array_idx] = (data[i] << 17);
 
-	uint32_t condensed_array[2 * 256 - 1] = { 0 };
-
-	uint32_t condensed_array_idx = 0;
-
-	for (int i =  0; i < DATA_SIZE; i++) {
-		if ((condensed_array[idx_array[data[i]]] >> 17) != data[i]) {
-			condensed_array[condensed_array_idx] = (data[i] << 17);
-
-			idx_array[data[i]] = condensed_array_idx++;
+			tree_index_array[data[i]] = (*tree_array_idx)++;
 		}
 
-		condensed_array[idx_array[data[i]]]++;
-
-		data_size++;
+		tree_array[tree_index_array[data[i]]]++;
 	}
-
-	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
-
-	printf("sorted array size - %i\n", condensed_array_idx);
 	
-	// uint16_t leaf_count = condensed_array_idx;
+	return i;
+}
 
-	for (int i = 0; i < condensed_array_idx; i++) {
-		for (int j = i + 1; j < condensed_array_idx; j++) {
-			// printf("i %i | j = %i\n", i, j);
-			if ((condensed_array[j] & FREQ_BIT) <= (condensed_array[i] & FREQ_BIT)) {
-				swap_u32(&condensed_array[j], &condensed_array[i]);
-				// printf("i %i | j = %i\n", i, j);
+void create_tree_nodes(uint32_t* tree_array, uint32_t* tree_array_idx) {
+	for (int i = 0; i < *tree_array_idx; i++) {
+		for (int j = i + 1; j < *tree_array_idx; j++) {
+			if ((tree_array[j] & FREQ_BIT) <= (tree_array[i] & FREQ_BIT)) {
+				swap_u32(&tree_array[j], &tree_array[i]);
 			}
 		}
 
 		if (i & 1) {
-			condensed_array[condensed_array_idx++] = ((i - 1) << 17) | (1 << 16) | ((condensed_array[i - 1] & FREQ_BIT) + (condensed_array[i] & FREQ_BIT));
+			tree_array[(*tree_array_idx)++] = ((i - 1) << 17) | (1 << 16) | ((tree_array[i - 1] & FREQ_BIT) + (tree_array[i] & FREQ_BIT));
 		}
 	}
+}
+
+int main() {
+	uint8_t data[DATA_SIZE] = "ABCDEFGGGABS";
+	
+	// for (uint32_t i = 0; i < DATA_SIZE; i++) {
+	// 	data[i] = i % 64;
+	// }
+
+	uint16_t id_array[256] = { 0 };
+	
+	uint16_t tree_index_array[256] = { 0 };
+	uint32_t tree_array[2 * 256 - 1] = { 0 };
+	uint32_t tree_array_idx = 0;
+	
+	uint32_t data_size = init_tree_array(data, tree_index_array, tree_array, &tree_array_idx);
+	
+	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
+
+	printf("sorted array size - %i\n", tree_array_idx);
+	
+	create_tree_nodes(tree_array, &tree_array_idx);
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 
-	printf("tree array size - %i\n", condensed_array_idx);
+	printf("tree array size - %i\n", tree_array_idx);
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 
-	for (int i = 0; i < condensed_array_idx; i++) {
+	for (int i = 0; i < tree_array_idx; i++) {
 		printf("%i: ", i);
-		if ((condensed_array[i] >> 16) & 1) {
-			printf("%i - %i : %i\n", condensed_array[i] >> 17, (condensed_array[i] >> 17) + 1, condensed_array[i] & FREQ_BIT);
+		if ((tree_array[i] >> 16) & 1) {
+			printf("%i - %i : %i\n", tree_array[i] >> 17, (tree_array[i] >> 17) + 1, tree_array[i] & FREQ_BIT);
 		} else {
-			printf("(%i) : %i\n", condensed_array[i] >> 17, condensed_array[i] & FREQ_BIT);
+			printf("(%c) : %i\n", tree_array[i] >> 17, tree_array[i] & FREQ_BIT);
 		}
 	}
 
-	for (int i = 0; i < condensed_array_idx; i++) {
-		printf("%i,", condensed_array[i]);
+	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
+
+	for (int i = 0; i < tree_array_idx; i++) {
+		printf("%i,", tree_array[i]);
 	}
 	printf("\n");
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 
-	uint16_t id_array[256] = { 0 };
-
-	traverse(condensed_array, id_array, condensed_array_idx - 1, 0, 1);
+	traverse(tree_array, id_array, tree_array_idx - 1, 0, 1);
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 
 	for (int i = 0; i < 256; i++) {
 		if (id_array[i]) {
-			printf("(%i) - ", i);
+			printf("(%c) - ", i);
 			if (id_array[i] > 511) {
 				printf(" [T] ");
 			}
@@ -161,84 +168,93 @@ int main() {
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 
-	// uint32_t val = result_array[data[index]];
+	uint64_t i_bit = 0;
+	uint8_t store_array[DATA_SIZE] = { 0 };
 
-	// uint32_t v = 511;
-
-	uint64_t bit_i = 0;
-	uint8_t store_array[1000000] = { 0 };
-	uint64_t len = 0;
-
-	{
-
-		for (uint32_t index = 0; index < DATA_SIZE; index++) {
-			for (uint32_t j = 0; j < 20; j++) {
-				if (id_array[data[index]] >> j == 1) {
-					for (uint8_t k = 0; k < j; k++) {
-						store_array[bit_i >> 3] |= ((id_array[data[index]] >> (j - k - 1)) & 1) << (8 - (bit_i % 8) - 1);
-						bit_i += 1;
-					}
+	for (uint32_t index = 0; index < DATA_SIZE; index++) {
+		for (uint32_t j = 0; j < 20; j++) {
+			if (id_array[data[index]] >> j == 1) {
+				for (uint8_t k = 0; k < j; k++) {
+					store_array[i_bit >> 3] |= ((id_array[data[index]] >> (j - k - 1)) & 1) << (8 - (i_bit % 8) - 1);
+					i_bit += 1;
 				}
 			}
 		}
-
-		for (int i = 0; i < (bit_i >> 3); i++) {
-			print_bin(store_array[i], 8);
-		}
-		
-		printf("\nsize = %lu\n", (bit_i - 1) >> 3);
-		
-		len = bit_i;
 	}
 
-	uint64_t pos = condensed_array_idx - 1;
-	printf("%s\n", data);
+	printf("compressed data\n\n");
+	for (int i = 0; i < (i_bit >> 3); i++) {
+		print_bin(store_array[i], 8);
+	}
+	printf("\n");
+
+	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
+
+	printf("uncompressed data\n\n");
 	for (int i = 0; i < DATA_SIZE; i++) {
 		print_bin(data[i], 8);
 	}
-	
 	printf("\n");
-	
-	uint8_t final_result[100000] = { 0 };
-	int final_result_index = 0;
 
-	for (int i = 0; i < len; i++) {
-		// printf("%i %i\n", pos, ((store_array[i >> 3] >> ((8 - (i % 8)) - 1)) & 1));
 
-		if ((condensed_array[pos] >> 16) & 1) {
-			pos = (condensed_array[pos] >> 17) + ((store_array[i >> 3] >> ((8 - (i % 8)) - 1)) & 1);
+	//* ----------------------| DECOMPRESSION | 
 
-			if (((condensed_array[pos] >> 16) & 1) == 0) {
-				final_result[final_result_index++] = condensed_array[pos] >> 17;
-				pos = condensed_array_idx - 1;
+	uint64_t pos = tree_array_idx - 1;
+	uint8_t final_result[DATA_SIZE] = { 0 };
+	uint32_t final_result_index = 0;
+
+	for (int i = 0; i < i_bit; i++) {
+		if ((tree_array[pos] >> 16) & 1) {
+			pos = (tree_array[pos] >> 17) + ((store_array[i >> 3] >> ((8 - (i % 8)) - 1)) & 1);
+
+			if (((tree_array[pos] >> 16) & 1) == 0) {
+				final_result[final_result_index++] = tree_array[pos] >> 17;
+				pos = tree_array_idx - 1;
 			}
 		}
-		// printf("%i\n", (store_array[i >> 3] >> ((8 - (i % 8)) - 1)) & 1);
 	}
 	
-	
-	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
-	
-	// printf("%s\n", final_result);
-	
-	for (int i = 0; i < final_result_index; i++) {
-		if (data[i] != final_result[i]) {
-			printf("\nincorrect!");
-			break;
-		} else {
-			printf("%c", final_result[i]);
+	for (int i = 0; i < data_size; i++) {
+		if (data[i] != final_result[i] || (final_result_index != data_size)) {
+			printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
+			printf("MATCH INCORRECT!\n");
+			printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
+
+			return 1;
 		}
 	}
-	printf("\n");
-	
-	// printf("%i - %i\n", final_result_index, data_size);
 	
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 	
-	float ratio = (1 - ((float)(bit_i >> 3) / (float)(data_size))) * 100;
+	float ratio = (1 - ((float)(i_bit >> 3) / (float)(data_size))) * 100;
 	printf("data_size = %i\n", data_size);
-	printf("comp_size = %lu\n", (bit_i >> 3));
+	printf("comp_size = %lu\n", (i_bit >> 3));
 	printf("compression ratio = %.2f%\n", ratio);
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 
+	// uint32_t stack[256 / 2] = { 0 };
+	// uint32_t stack_ptr = 0;
+	// uint32_t curr_pos = tree_array_idx - 1;
+	// u_int64_t id = 0;
+	
+	// while (1) {
+	// 	if ((tree_array[curr_pos] >> 16) & 1) {
+	// 		stack[stack_ptr++] = (tree_array[curr_pos] >> 16) | 1;
+	// 	} else {
+	// 		curr_pos = stack[--stack_ptr] >> 1;
+	// 	}
+	// }
+
+	// for (int i = 0; i < 256; i++) {
+	// 	if (id_array[i]) {
+	// 		printf("(%i) - ", i);
+	// 		if (id_array[i] > 511) {
+	// 			printf(" [T] ");
+	// 		}
+	// 		print_bin(id_array[i], 0);
+	// 		printf("\n");
+	// 	}
+	// }
+
+	// printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 }
