405fd9d9537d14f7ec5b14c1a85092ae096e5495
Krishnadev K <89983190+laperex@users.noreply.github.com>
2024-02-28 20:09:03 +0530

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/laperex/Programming/C++/compressor
-# Build directory: /home/laperex/Programming/C++/compressor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
