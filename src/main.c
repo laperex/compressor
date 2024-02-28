74e18d6b67db8f09e76f8b4bfafb1545104bfe86
laperex <krishnadev7777@gmail.com>
2024-02-28 22:35:48 +0530

    comment

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// to print data as char
+// remove when using raw bytes ( non char based data )
 #define PRINT_AS_CHAR
 
-#define FREQ_BIT ((256 * 256) - 1)
+// change depending on data
 #define DATA_SIZE (12)
 
 
@@ -14,6 +16,7 @@
 	#define NODE_PRINT "%i"
 #endif
 
+#define FREQ_BIT ((256 * 256) - 1)
 
 void swap_u32(uint32_t* a, uint32_t* b) {
 	uint32_t tmp = *a;
@@ -101,6 +104,13 @@ uint32_t init_tree_array(uint8_t* data, uint16_t* tree_index_array, uint32_t* tr
 }
 
 void create_tree_nodes(uint32_t* tree_array, uint32_t* tree_array_idx) {
+	// typical selection sort algorithm
+	// tree nodes are inserted during the runtime of the loop to increase performance
+
+	// O((N * (N - 1)) / 2) performance
+	// where N is the total size of the array including the tree nodes and tree leafs at the end of the loop
+	// N = 2n - 1 where n is the total count of leaf nodes
+	
 	for (int i = 0; i < *tree_array_idx; i++) {
 		for (int j = i + 1; j < *tree_array_idx; j++) {
 			if ((tree_array[j] & FREQ_BIT) <= (tree_array[i] & FREQ_BIT)) {
@@ -127,10 +137,13 @@ int main() {
 	uint32_t tree_array[2 * 256 - 1] = { 0 };
 	uint32_t tree_array_idx = 0;
 	
+	// creates a contiguous array, mapping each unique byte to their frequency count.
+	// initialize the tree array with the tree leafs sorted in ascending order of their frequency
 	uint32_t data_size = init_tree_array(data, tree_index_array, tree_array, &tree_array_idx);
 	
 	uint32_t leaf_count = tree_array_idx;
-	
+
+	// create tree nodes
 	create_tree_nodes(tree_array, &tree_array_idx);
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
@@ -160,6 +173,8 @@ int main() {
 
 	printf("huffman tree: tree view\n\n");
 
+	// recursively travese the tree array and determine the huffman code
+	// id_array - stores the huffman code, each unique byte acts as index to this array
 	traverse(tree_array, id_array, tree_array_idx - 1, 0, 1);
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
@@ -178,10 +193,13 @@ int main() {
 	}
 
 	printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
+	
+	//* ----------------------| COMPRESSION | 
 
 	uint64_t i_bit = 0;
 	uint8_t store_array[DATA_SIZE] = { 0 };
 
+	// convert the bytes in the given data to huffman codes of varying sizes
 	for (uint32_t index = 0; index < DATA_SIZE; index++) {
 		for (uint32_t j = 0; j < 20; j++) {
 			if (id_array[data[index]] >> j == 1) {
@@ -269,3 +287,232 @@ int main() {
 
 	// printf("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- \n");
 }
+
+
+// `timescale 1ns / 1ps
+// //////////////////////////////////////////////////////////////////////////////////
+// // Company: 
+// // Engineer: 
+// // 
+// // Create Date: 02/18/2024 08:16:08 PM
+// // Design Name: 
+// // Module Name: interface
+// // Project Name: 
+// // Target Devices: 
+// // Tool Versions: 
+// // Description: 
+// // 
+// // Dependencies: 
+// // 
+// // Revision:
+// // Revision 0.01 - File Created
+// // Additional Comments:
+// // 
+// //////////////////////////////////////////////////////////////////////////////////
+
+// module memory #(parameter WIDTH = 32, parameter SIZE = 100) (clk, reset, wr_en, i_address_wr, i_data_wr, o_data_rd);
+// 	input clk;
+// 	input reset;
+
+// 	input [$clog2(SIZE - 1): 0] i_address_wr;
+
+// 	input wr_en;
+// 	input [WIDTH - 1: 0] i_data_wr;
+
+// 	output [WIDTH - 1: 0] o_data_rd;
+
+// 	reg tree_array[0: SIZE - 1] = "";
+
+// 	always @(posedge clk) begin
+// 		if (wr_en) begin
+// 			tree_array[i_address_wr] <= i_data_wr;
+// 		end
+// 	end
+// endmodule
+
+
+// `define BRCH_NEXT [9 + 17: 0 + 16]
+
+// // `define NODE_TYPE [18]
+// `define NODE_FREQ [16 - 1: 0]
+
+// `define LEAF_DATA [8 + 17: 0 + 17]
+
+// `define ARR_TYPE [15: 0]
+// `define ARR_INDX [15: 0]
+
+// module interface(clk, reset, btn, led);
+// 	input clk;
+// 	input reset;
+// 	input [3: 0] btn;
+	
+// 	output [15: 0] led;
+	
+// 	parameter DATA_MAX_SIZE = 12;
+
+// 	reg [1: 0] op_state = 0;
+
+// 	reg [7: 0] data[0: DATA_MAX_SIZE - 1] = "ABCDEFGGGABS";
+
+// 	// reg [7: 0] tree_index_array[0: 255] = "";
+
+// 	reg [8: 0] tree_array_idx = 0;
+// 	reg [7: 0] next_tree_array_idx = 1;
+// 	reg [7: 0] prev_tree_array_idx = 0;
+// 	// reg [32 - 1: 0] tree_array[0: 512 - 1] = "";
+
+// 	reg [7: 0] i = 0;
+
+// 	// wire [7: 0] _data = data[i];
+// 	// wire [7: 0] _data_i = tree_index_array[data[i]][7: 0];
+
+// 	reg ta_en;
+// 	reg [8: 0] ta_address;
+// 	wire [32: 0] ta_data_wr;
+// 	wire [32: 0] ta_data_rd;
+	
+// 	reg idx_ta_en;
+// 	reg [7: 0] idx_ta_address;
+// 	wire [9: 0] idx_ta_data_wr;
+// 	wire [9: 0] idx_ta_data_rd;
+
+// 	memory #(32, 256 * 2 - 1) m_tree_array(clk, reset, ta_en, ta_address, ta_data_wr, ta_data_rd);
+// 	memory #(9, 256) m_tree_index_array(clk, reset, idx_ta_en, idx_ta_address, idx_ta_data_wr, idx_ta_data_rd);
+	
+// 	reg negedge_signal = 0;
+
+// 	always @(posedge clk && op_state == 0) begin
+// 		if (i == DATA_MAX_SIZE - 1) begin
+// 			i <= 0;
+
+// 			op_state <= 1;
+// 		end else begin
+// 			i <= i + 1;
+// 		end
+
+// 		if (tree_arrays[data[i]]`LEAF_DATA != tree_index_array[data[i]][7: 0] || (i == 0 && tree_index_array[data[i]][7: 0] == 0)) begin
+// 			tree_array[tree_array_idx]`LEAF_DATA <= tree_index_array[data[i]][7: 0];
+// 			tree_array[tree_array_idx]`NODE_FREQ <= 1;
+
+// 			tree_index_array[tree_index_array[data[i]][7: 0]] <= tree_array_idx;
+
+// 			tree_array_idx <= next_tree_array_idx;
+// 			next_tree_array_idx <= next_tree_array_idx + 1;
+// 			prev_tree_array_idx <= tree_array_idx;
+// 		end else begin
+// 			tree_array[data[i]]`NODE_FREQ <= tree_array[data[i]]`NODE_FREQ + 1;
+// 		end
+// 	end
+
+// 	// always @(negedge clk && op_state == 0 && begin_signal) begin
+		
+// 	// end
+
+// 	// reg [7: 0] j = 0;
+// 	// reg [7: 0] i_next = 1;
+// 	// reg [7: 0] i_prev = 0;
+
+// 	// assign led[15: 0] = tree_array[14][15: 0];
+
+// 	// always @(posedge clk && op_state == 1) begin
+// 	// 	if (j == prev_tree_array_idx) begin
+// 	// 		if (i[0] == 1) begin
+// 	// 			tree_array[tree_array_idx]`NODE_FREQ <= tree_array[i_prev]`NODE_FREQ + tree_array[i]`NODE_FREQ;
+// 	// 			tree_array[tree_array_idx]`BRCH_NEXT <= (i_prev << 1) | 1;
+
+// 	// 			prev_tree_array_idx <= tree_array_idx;
+// 	// 			next_tree_array_idx <= next_tree_array_idx + 1;
+// 	// 			tree_array_idx <= next_tree_array_idx;
+// 	// 		end
+
+// 	// 		if (i_next == prev_tree_array_idx) begin
+// 	// 			if (i_next == i) begin
+// 	// 				i <= 0;
+// 	// 				j <= 0;
+// 	// 				i_next <= 1;
+// 	// 				i_prev <= 0;
+
+// 	// 				op_state <= 2;
+// 	// 			end else begin
+// 	// 				i <= i_next;
+// 	// 				i_prev <= i;
+// 	// 			end
+// 	// 		end else begin
+// 	// 			i_next <= i_next + 1;
+
+// 	// 			i <= i_next;
+// 	// 			j <= i_next + 1;
+
+// 	// 			i_prev <= i;
+// 	// 		end
+// 	// 	end else begin
+// 	// 		j <= j + 1;
+// 	// 	end
+
+// 	// 	if (tree_array[j]`NODE_FREQ <= tree_array[i]`NODE_FREQ) begin
+// 	// 		tree_array[j] <= tree_array[i];
+// 	// 		tree_array[i] <= tree_array[j];
+// 	// 	end
+// 	// end
+
+// 	// always @(posedge clk && op_state == 1) begin
+// 	// 	if (i_next == prev_tree_array_idx) begin
+// 	// 		if (i_next == i) begin
+// 	// 			i <= 0;
+				
+// 	// 			i_next <= 1;
+// 	// 			i_prev <= 0;
+
+// 	// 			op_state <= 2;
+// 	// 		end else begin
+// 	// 			i <= i_next;
+// 	// 			i_prev <= i;
+// 	// 		end
+// 	// 	end else begin
+// 	// 		i_next <= i_next + 1;
+// 	// 		i <= i_next;
+// 	// 		i_prev <= i;
+// 	// 	end
+// 	// end
+
+// 	// reg [32: 0] swap_val[0: 1] = 0;
+// 	// // reg [32: 0] tmp = 0;
+// 	// wire [256: 0] cmp = 0;
+	
+// 	// assign led = tree_array[0][15: 0];
+	
+// 	// initial begin
+// 	// 	// tmp = 0;
+// 	// 	cmp = 0;
+// 	// end
+
+// 	// generate
+// 	// genvar _j;
+// 	// for (_j = 1; _j < 254; _j = _j + 1) begin
+// 	// 	assign cmp[_j] = (tree_array[_j + i]`NODE_FREQ <= tree_array[i]`NODE_FREQ) ? 1: 0;
+		
+// 	// 	always @(posedge clk && op_state == 1) begin
+// 	// 		if (_j + i <= prev_tree_array_idx) begin
+// 	// 			if (tree_array[_j + i]`NODE_FREQ <= tree_array[i]`NODE_FREQ) begin
+// 	// 				swap_val[0] = tree_array[i];
+// 	// 				swap_val[1] = tree_array[_j + i];
+// 	// 				// tree_array[i] = tree_array[_j + i];
+// 	// 				// tree_array[_j + i] = tmp;
+// 	// 			end
+// 	// 		end
+// 	// 	end
+// 	// end
+// 	// endgenerate
+
+// 	// always @(posedge clk && op_state == 1) begin
+// 	// 	if (i[0] == 1) begin
+// 	// 		tree_array[tree_array_idx]`NODE_FREQ <= tree_array[i_prev]`NODE_FREQ + tree_array[i]`NODE_FREQ;
+// 	// 		tree_array[tree_array_idx]`BRCH_NEXT <= (i_prev << 1) | 1;
+
+// 	// 		prev_tree_array_idx <= tree_array_idx;
+// 	// 		next_tree_array_idx <= next_tree_array_idx + 1;
+// 	// 		tree_array_idx <= next_tree_array_idx;
+// 	// 	end
+// 	// end
+// endmodule
+
